97d84d3aa8c8f19dc0dd594061dc45e9dc062504
Ian Boucher <i.a.boucher@gmail.com>
2016-07-18 21:41:12 +0100

    Implement 1st iteration of polymorphic favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,31 +1,30 @@
 class FavoritesController < ApplicationController
 
-# before_action :set_favoritable_type
+before_action :set_favoritable_type
 
   def create
-    favorite = current_user.favorites.create(favoritable: params[:favoritable])
+    favorite = current_user.favorites.build(favoritable: @favoritable)
 
     if favorite.save
       flash[:notice] = "Artwork favorited"
     else
       flash[:notice] = "Favoriting failed"
     end
-    redirect_to @favoritable
+    redirect_to :back
   end
 
   def destroy
-    favorite = current_user.favorites.find(params[:id])
+    favorite = current_user.favorites.find_by(favoritable: @favoritable)
 
     if favorite.destroy
       flash[:notice] = "Artwork unfavorited"
     else
       flash[:notice] = "Unfavoriting failed"
     end
-    redirect_to @favoritable
+    redirect_to :back
   end
 
   def set_favoritable_type
-    binding.pry
     if params.has_key?("artwork_id")
       @favoritable = Artwork.find(params[:artwork_id])
     else
